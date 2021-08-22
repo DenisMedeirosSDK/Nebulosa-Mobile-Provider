ed1aeca85801ef469c909896865e4cc15e898d11
DenisMedeirosSDK <contato.denismedeiros@hotmail.com>
2021-08-22 13:38:03 -0300

    feat: add styled component and use theme

---
+++ src/Global/styles/theme.ts
@@ -0,0 +1,23 @@
+export default {
+  colors: {
+    primary: '#EE926F',
+    success: '#03B252',
+    problem: '#DC1637',
+
+    background_light: '#FFFFFF',
+    background_semi: '#F4F5F6',
+    background_dark: '#1B1B1F',
+
+    text_light: '#FFFFFF',
+    text_semi: '#AEAEB3',
+    text_dark: '#47474D',
+  },
+  fonts: {
+    archivo_400: '',
+    archivo_500: '',
+    archivo_600: '',
+    inter_400: '',
+    inter_500: '',
+    inter_600: '',
+  },
+};
